ffaf8a276da3372ac308e14f8a535b9237933002
lxdnz254 <lxdnz34@gmail.com>
2016-04-16 12:22:42 +1200

    Removed Log.v tags so not to clutter Logcat

--- app/src/main/java/com/lxdnz/nz/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/lxdnz/nz/sunshine/app/ForecastFragment.java
@@ -208,9 +208,6 @@ public class ForecastFragment extends Fragment {
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
 
-            for (String s : resultStrs) {
-                Log.v(LOG_TAG, "Forecast entry: " + s);
-            }
             return resultStrs;
 
         }
@@ -258,8 +255,6 @@ public class ForecastFragment extends Fragment {
 
                 URL url = new URL(builtUri.toString());
 
-                Log.v(LOG_TAG, "Built Uri:" + builtUri.toString());
-
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -287,8 +282,6 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
-
-                Log.v(LOG_TAG, "Forecast JSON String:" + forecastJsonStr);
 
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
