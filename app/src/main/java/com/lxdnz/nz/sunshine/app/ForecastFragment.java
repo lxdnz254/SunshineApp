812bfe56409218ddd180f03d111a4005d43f7441
lxdnz254 <lxdnz34@gmail.com>
2016-04-16 16:21:48 +1200

    Lesson 3 .. Send Explict Intent with forecast data from Forecast Fragment and receive at the renamed "Detail Fragment"

--- app/src/main/java/com/lxdnz/nz/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/lxdnz/nz/sunshine/app/ForecastFragment.java
@@ -1,6 +1,7 @@
 package com.lxdnz.nz.sunshine.app;
 
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -16,7 +17,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -115,7 +115,9 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
             }
         });
 
