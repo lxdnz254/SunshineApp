5f291efe1778e2abb922c96a1bea56f9d57a81c4
lxdnz254 <lxdnz34@gmail.com>
2016-05-10 18:15:32 +1200

    Lesson 6: SyncAdapter has time interval (3 hours)

--- app/src/main/java/com/lxdnz/nz/sunshine/app/MainActivity.java
+++ app/src/main/java/com/lxdnz/nz/sunshine/app/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.lxdnz.nz.sunshine.app.sync.SunshineSyncAdapter;
+
 
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback{
 
@@ -44,6 +46,8 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        SunshineSyncAdapter.initializeSyncAdapter(this);
 
     }
 
