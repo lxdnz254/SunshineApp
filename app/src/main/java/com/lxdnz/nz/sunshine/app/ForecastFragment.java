8c7b8507dd5e7527a2740fc75e04edbbcb8a2cb8
lxdnz254 <lxdnz34@gmail.com>
2016-06-18 12:23:34 +1200

    Show the User status - get status from SharedPreferences

--- app/src/main/java/com/lxdnz/nz/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/lxdnz/nz/sunshine/app/ForecastFragment.java
@@ -321,7 +321,9 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
+        if (key.equals(getString(R.string.pref_location_status_key))) {
+            updateEmptyView();
+        }
     }
 }
 
