c0d3f422d195a38e2cc6cc71b229e5488a26485d
lxdnz254 <lxdnz34@gmail.com>
2016-06-29 12:07:56 +1200

    Widget get real data from SyncAdapter

---
+++ app/src/main/java/com/lxdnz/nz/sunshine/app/widget/TodayWidgetIntentService.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2016. The Android Open Source Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lxdnz.nz.sunshine.app.widget;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import com.lxdnz.nz.sunshine.app.MainActivity;
+import com.lxdnz.nz.sunshine.app.R;
+import com.lxdnz.nz.sunshine.app.Utility;
+import com.lxdnz.nz.sunshine.app.data.WeatherContract;
+
+/**
+ * IntentService which handles updating all Today widgets with the latest data
+ * Created by alex on 29/06/16.
+ */
+public class TodayWidgetIntentService extends IntentService {
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP
+    };
+    // these indices must match the projection
+    private static final int INDEX_WEATHER_ID = 0;
+    private static final int INDEX_SHORT_DESC = 1;
+    private static final int INDEX_MAX_TEMP = 2;
+
+    public TodayWidgetIntentService() {
+        super("TodayWidgetIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // Retrieve all of the Today widgets ids: these are the widgets we need to update
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
+                TodayWidgetProvider.class));
+
+        // Get today's data from the ContentProvider
+        String location = Utility.getPreferredLocation(this);
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                location, System.currentTimeMillis());
+        Cursor data = getContentResolver().query(weatherForLocationUri, FORECAST_COLUMNS, null,
+                null, WeatherContract.WeatherEntry.COLUMN_DATE + " ASC");
+        if (data == null){
+            return;
+        }
+        if(!data.moveToFirst()){
+            data.close();
+            return;
+        }
+
+        // Get the weather data from the cursor
+        int weatherId = data.getInt(INDEX_WEATHER_ID);
+        int weatherArtResourceId = Utility.getArtResourceForWeatherCondition(weatherId);
+        String description = data.getString(INDEX_SHORT_DESC);
+        double maxTemp = data.getDouble(INDEX_MAX_TEMP);
+        String formattedMaxTemp = Utility.formatTemperature(this, maxTemp);
+        data.close();
+
+        // perform this loop for each Today widget
+        for (int appWidgetId: appWidgetIds) {
+            int layoutId = R.layout.widget_today_small;
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+
+            // Add data to the RemoteViews
+            views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
+            // ContentDescriptions were only added for RemoteViews in ICS MR1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(views, description);
+            }
+            views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemp);
+
+            // Create an Intent to launch MainActivity
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Tell AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_icon, description);
+    }
+}
--- app/src/main/java/com/lxdnz/nz/sunshine/app/widget/TodayWidgetProvider.java
+++ app/src/main/java/com/lxdnz/nz/sunshine/app/widget/TodayWidgetProvider.java
@@ -22,50 +22,40 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import com.lxdnz.nz.sunshine.app.MainActivity;
 import com.lxdnz.nz.sunshine.app.R;
 import com.lxdnz.nz.sunshine.app.Utility;
+import com.lxdnz.nz.sunshine.app.sync.SunshineSyncAdapter;
 
 /**
  * Provider for a widget showing today's weather
+ *
+ * Delegate widget updating to {@link TodayWidgetIntentService} to ensure that
+ * data retrieval is done on a background thread
+ *
  * Created by alex on 29/06/16.
  */
 public class TodayWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        int weatherArtResourceId = R.drawable.art_clear;
-        String description = "Clear";
-        double maxTemp = 24;
-        String formattedMaxTemp = Utility.formatTemperature(context, maxTemp);
-
-        // perform this loop for each Today widget
-        for (int appWidgetId: appWidgetIds) {
-            int layoutId = R.layout.widget_today_small;
-            RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
-
-            // Add data to the RemoteViews
-            views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
-            // ContentDescriptions were only added for RemoteViews in ICS MR1
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
-                setRemoteContentDescription(views, description);
-            }
-            views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemp);
-
-            // Create an Intent to launch MainActivity
-            Intent launchIntent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
-            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
-
-            // Tell AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
     }
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
-    private void setRemoteContentDescription(RemoteViews views, String description) {
-        views.setContentDescription(R.id.widget_icon, description);
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                          int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            context.startService(new Intent(context, TodayWidgetIntentService.class));
+        }
     }
 }
